c711f77e790113cc6d24e8c110e89aea5bbdbe0b
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-10-08 10:01:15 +0000

    Add function to get data string

--- run.py
+++ run.py
@@ -12,7 +12,15 @@ SCOPES_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPES_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figure imput from the user
+    """
+    print("Please enter sales datat from the last market.")
+    print("Data should be six numbers seperated by commas")
+    print("Example: 10,20,30,40,50,60\n")
 
-data = sales.get_all_values()
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
